223d3c1ae7a328d8529f252ee406eca68df21be3
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-10-21 17:33:26 +0700

    create user model

--- configs/db.go
+++ configs/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"example.com/gin-backend-api/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -17,7 +18,10 @@ func Connection() {
 	if err != nil {
 		fmt.Println("ไม่สามารถติดต่อกับ Database Server ได้")
 		fmt.Println(err.Error())
+		panic(err)
 	}
+
+	db.AutoMigrate(&models.User{})
 
 	fmt.Println("ติดต่อฐานข้อมูลสำเร็จ")
 	DB = db
